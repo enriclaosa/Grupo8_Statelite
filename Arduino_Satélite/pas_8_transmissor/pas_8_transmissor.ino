34565d69d959e840c5d3b626ccee285e708a0996
aniolcastany <aniol.castany@estudiantat.upc.edu>
2025-11-04 19:08:33 +0100

    Update pas_8_transmissor.ino

--- Arduino_Satélite/pas_8_transmissor/pas_8_transmissor.ino
+++ Arduino_Satélite/pas_8_transmissor/pas_8_transmissor.ino
@@ -5,9 +5,12 @@
 DHT dht(DHTPIN, DHTTYPE);
 SoftwareSerial mySerial(10, 11); // RX, TX 
 #define LedVerd 6
-unsigned long nextMillis;
-const unsigned long interval = 500;
+unsigned long nextMillis1;
+const unsigned long interval1 = 500;
+unsigned long nextMillis2;
+const unsigned long interval2 = 3000;
 unsigned long nextTimeoutHT;
+const unsigned long interval3 = 5000;
 bool esperandoTimeout = false;
 bool enviarDatos = true;
 String mensaje;
@@ -16,15 +19,17 @@ void setup() {
   mySerial.begin(9600);
   Serial.begin(9600);
   dht.begin();
-  pinMode(6, OUTPUT);
-  nextMillis = millis() + interval;
+  pinMode(LedVerd, OUTPUT);
+  nextMillis1 = millis() + interval1;
+  nextMillis2 = millis() + interval2;
+  nextTimeoutHT = millis() + interval3;
 }
 
 void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   if (Serial.available() > 0) {
-    mensaje = Serial.readString();
+    mensaje = Serial.readStringUntil('\n');
   }
   if(mensaje == "Parar")
     enviarDatos = false;
@@ -33,19 +38,29 @@ void loop() {
   if(enviarDatos == true){
     if (isnan(h) || isnan(t)){
       mySerial.println("Error al leer el sensor DHT11");
+      Serial.println (mensaje);//mirar
       esperandoTimeout = true;
-      nextTimeoutHT = millis() + 5000;
+      if (!esperandoTimeout){
+        esperandoTimeout = true
+        nextTimeoutHT = millis() + interval3;
+      }
     }
     else {
-      esperandoTimeout = false;
-      digitalWrite(6, HIGH);
-      mySerial.print(h);
-      mySerial.print(" ");
-      mySerial.println(t);
-    if (millis() >= nextMillis){
-      digitalWrite(6, LOW);
-    }
-    delay (3000);
-    }
+      if (millis() >= nextMillis2){
+        esperandoTimeout = false;
+        digitalWrite(LedVerd, HIGH);
+        mySerial.print(h);
+        mySerial.print(" ");
+        mySerial.println(t);
+        if (millis() >= nextMillis1){
+          digitalWrite(LedVerd, LOW);
+          nextMillis1 = millis() + interval1;
+        }
+        nextMillis2 = millis() + interval2;
+        }
+        
+      }
+    if(esperandoTimeout && (millis() >= nextTimeoutHT))
+      mySerial.println("Fallo");
   }
 }
