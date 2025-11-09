06b0e7e628f4fcf9ab28751d46ee1210fd1b9213
gjane570 <gala.jane@estudiantat.upc.edu>
2025-11-09 17:24:22 +0100

    5.1_satelite.ino

--- Arduino_Satélite/pas_8_transmissor/5.1_satelite.ino
+++ Arduino_Satélite/pas_8_transmissor/5.1_satelite.ino
@@ -7,12 +7,13 @@ void loop() {
   if(mySerial.available()){
     String comando = mySerial.readStringUntil('\n');
     comando.trim();
-    int fin = comando.indexOf(' ',0);// Troba la posició del primer espai en la cadena
+    int fin = comando.indexOf(':',0);// Troba la posició del primer : en la cadena
     int codigo = comando.substring(0, fin).toInt();// Extreu el codi (del principi fins a la posició espai) i el converteix a int
     int inicio = fin+1;// Marca on comença la informació després del codi
     if(codigo == 1){
-      fin=comando.indexOf(' ',inicio);// Busca si hi ha un altre espai després del valor
-      intervalHT = comando.substring(inicio, fin).toInt();// El valor que hi ha després de l'espai el passa a int i l'assigna a l'intervalHT
+      fin=comando.indexOf(':',inicio);// Busca si hi ha un altre : després del valor
+      if (fin == -1) fin = comando.length();  // Si no hi ha un altre valor, agafa fins el final final
+        intervalHT = comando.substring(inicio, fin).toInt() * 1000;// El valor que hi ha després de l'espai el passa a int i l'assigna a l'intervalHT + multiplica per 1000 per passar a milis
     }
     else if(codigo == 2){
 //orientacio sensor distancia
