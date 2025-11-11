e792deede2ee9e10a27b57ea07e57f99fb102d23
gjane570 <gala.jane@estudiantat.upc.edu>
2025-11-11 08:53:41 +0100

    Programa_Receptor.ino

--- Arduino_Tierra/Programa_Receptor/Programa_Receptor.ino
+++ Arduino_Tierra/Programa_Receptor/Programa_Receptor.ino
@@ -76,6 +76,11 @@ void loop() {
       falloDHT = true;
       digitalWrite(led_fallo_datostemp, HIGH);
     }
+    else if(codigo == 4){
+      fin=comando.indexOf(' ',inicio);
+      if(fin == -1) fin = comando.length();
+      intervalHT = comando.substring(inicio, fin).toInt() * 1000;
+    }
     else {
     // Verificar palabra "Fallo" en la cadena recibida (case sensitive)
     if (data.indexOf("Fallo") >= 0) {
