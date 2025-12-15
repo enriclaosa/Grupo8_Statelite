3d732796f8fcf7705a5086e6051e233caa956b3c
aniolcastany <aniol.castany@estudiantat.upc.edu>
2025-12-15 14:33:39 +0100

    Update Display afegit.ino

--- Arduino_Tierra/Programa_Receptor/Display afegit.ino
+++ Arduino_Tierra/Programa_Receptor/Display afegit.ino
@@ -2,7 +2,7 @@
 
 SoftwareSerial mySerial(10, 11); // RX, TX (azul, naranja)
 
-//const int led_rec = 8;                // LED rojo: indica recepción de datos
+const int led_rec = 8;                // LED rojo: indica recepción de datos
 const int led_fallo_datostemp = 6;   // LED azul: indica fallo en datos de temperatura
 const int led_timeout = 4;          // LED amarillo: indica timeout (5s sin datos)
 const int buzzer = 7;          
@@ -23,6 +23,21 @@ bool falloDistancia = false;
 unsigned long buzzer_on_time = 0;          // para limitar duración del pitido
 const unsigned long buzzer_duration = 1000; // 1 segundo de duración
 bool buzzer_active = false;                // Estado del buzzer
+
+const int segPins[7] = {2, 3, 4, 5, 6, 7, 8};
+
+void mostrar8() {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segPins[i], LOW);   // engegar tots els segments
+  }
+}
+
+void apagarDisplay() {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segPins[i], HIGH);  // apagar tots els segments
+  }
+}
+
 
 // Función para activar el buzzer por 1 segundo
 void activarBuzzer() {
@@ -68,22 +83,12 @@ unsigned long nextUpdate; // Time in milliseconds when the next orbit simulation
 double real_orbital_period;  // Real orbital period of the satellite (seconds)
 double r;  // Total distance from Earth's center to satellite (meters)
 
-const int segPins[7] = {2, 3, 4, 5, 6, 7, 8};
-
-
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 
-  //pinMode(led_rec, OUTPUT);
-  //digitalWrite(led_rec, LOW);
-
-  for (int i = 0; i < 7; i++) {
-    pinMode(segPins[i], OUTPUT);
-  }
-  for (int i = 0; i < 7; i++) {
-      digitalWrite(segPins[i], HIGH);   // ànode comú: LOW encén el segment
-  }
+  pinMode(led_rec, OUTPUT);
+  digitalWrite(led_rec, LOW);
 
   pinMode(led_fallo_datostemp, OUTPUT);
   digitalWrite(led_fallo_datostemp, LOW);
@@ -99,6 +104,15 @@ void setup() {
   nextUpdate = MILLIS_BETWEEN_UPDATES;
   r = R_EARTH + ALTITUDE;
   real_orbital_period = 2 * PI * sqrt(pow(r, 3) / (G * M));
+  // Display amb un 8
+  for (int i = 0; i < 7; i++) {
+    pinMode(segPins[i], OUTPUT);
+  }
+
+  // Nombre 8 -> tots els segments ON
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segPins[i], HIGH);  // càtode comú: HIGH encén el segment
+  }
 }
 
 void loop() {
@@ -161,19 +175,15 @@ void loop() {
       Serial.print(" °C, Media últimas "); Serial.print(contadorTemp);
       Serial.print(": "); Serial.println(mediaTemperaturas);
       }
-      // Nombre 8 -> tots els segments ON
-    for (int i = 0; i < 7; i++) {
-      digitalWrite(segPins[i], LOW);   // ànode comú: LOW encén el segment
-    }
+
     //digitalWrite(led_rec, HIGH);
+    mostrar8();
     led_rec_on = true;
     led_rec_off_time = millis() + led_on_interval;
     digitalWrite(led_fallo_datostemp, LOW);
 
-      for (int i = 0; i < 7; i++) {
-        digitalWrite(segPins[i], LOW);   // ànode comú: LOW encén el segment
-      }
       //digitalWrite(led_rec, HIGH);  // LED recepción datos on (igual que antes)
+      mostrar8();
       led_rec_on = true;
       led_rec_off_time = millis() + led_on_interval;
       digitalWrite(led_fallo_datostemp, LOW);
@@ -211,10 +221,8 @@ void loop() {
       } else {
         digitalWrite(led_fallo_datostemp, LOW);
         // LED recepción datos ON y programar apagado
-        for (int i = 0; i < 7; i++) {
-          digitalWrite(segPins[i], LOW);   // ànode comú: LOW encén el segment
-        }
         //digitalWrite(led_rec, HIGH);
+        mostrar8();
         led_rec_on = true;
         led_rec_off_time = millis() + led_on_interval;
       }
@@ -223,10 +231,8 @@ void loop() {
   
   // Apagar LED recepción después del intervalo
   if (led_rec_on && millis() >= led_rec_off_time && digitalRead(led_fallo_datostemp) == LOW) {
-    for (int i = 0; i < 7; i++) {
-      digitalWrite(segPins[i], HIGH);   // ànode comú: LOW encén el segment
-    }
     //digitalWrite(led_rec, LOW);
+    apagarDisplay()
     led_rec_on = false;
   }
 
